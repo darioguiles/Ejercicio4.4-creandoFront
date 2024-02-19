991039c2c717df22ea977fc5f8240df9b5f129f8
darioguiles <dario.guilescuesta@hotmail.com>
2024-02-19 12:57:17 +0100

    primer commit, Tarea hecha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {CategoriaModule} from "./app-videoclub/categoria/categoria.module";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CategoriaModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
